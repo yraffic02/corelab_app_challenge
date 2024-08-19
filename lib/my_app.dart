a7a39adf71581fb8edcd7488cd059abae8c3fcc8
yraffic02 <yraffic02@gmail.com>
2024-08-19 17:14:28 -0300

    feat: criado arquivo my app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,15 +1,18 @@
 import 'package:corelab_app_challenge/pages/home/home_page.dart';
+import 'package:corelab_app_challenge/shared/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Shop',
+      title: 'Teste CoreLab',
       theme: ThemeData(
-        primaryColor: Color(0xFF00B4CC),
+        primaryColor: const Color(0xFF00B4CC),
       ),
       home: HomeScreen(),
+      initialRoute: '/',
+      routes: Constants.buildRoutes(context),
     );
   }
 }
