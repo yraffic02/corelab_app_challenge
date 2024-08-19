27e7e1f3d9bef954e6c8ad57fe8d2c9d8cadbbff
yraffic02 <yraffic02@gmail.com>
2024-08-19 17:14:55 -0300

    chore: adcionado card do produto

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:corelab_app_challenge/shared/ProductCard/custom_product_card.dart';
+import 'package:corelab_app_challenge/shared/CustomLayout/custom_layout.dart';
+import 'package:corelab_app_challenge/shared/CustomProductCard/custom_product_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,74 +8,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF00B4CC),
-        leading: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Buscar',
-              fillColor: Colors.white,
-              filled: true,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: BorderSide.none,
-              ),
-              suffixIcon: Icon(Icons.search),
-            ),
-          ),
+    return const CustomLayout(
+        child: Column(
+      children: [
+        ProductCard(
+          imageUrl: 'https://via.placeholder.com/100',
+          productName: 'Condicionador Ácido Fortificante 37% - AlPrime',
+          price: 2900,
+          info: 'em até 12x de R\$ 24,17',
+          isNew: true,
+          discountPercentage: 10,
         ),
-        leadingWidth: double.infinity,
-      ),
-      body: const Column(
-        children: [
-          ProductCard(
-            imageUrl: 'https://via.placeholder.com/100',
-            productName: 'Condicionador Ácido Fortificante 37% - AlPrime',
-            price: 'R\$ 290,00',
-            installmentInfo: 'em até 12x de R\$ 24,17',
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            label: 'Categorias',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box),
-            label: 'Anunciar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favoritos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Conta',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xFF00B4CC),
-        unselectedItemColor: Color(0xFF7C9094),
-        onTap: _onItemTapped,
-      ),
-    );
+      ],
+    ));
   }
 }
