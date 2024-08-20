947e7654dcb6ec0980105a18d8cac6a98b541f75
yraffic02 <yraffic02@gmail.com>
2024-08-20 14:46:06 -0300

    chore: add nova rota categoria

--- lib/shared/utils/constants.dart
+++ lib/shared/utils/constants.dart
@@ -1,8 +1,9 @@
+import 'package:corelab_app_challenge/pages/categories/categoies_page.dart';
 import 'package:flutter/material.dart';
 
 class Constants {
   static Map<String, Widget Function(BuildContext)> buildRoutes(
       BuildContext context) {
-    return {};
+    return {'/Categorias': (context) => const CategoriesPage()};
   }
 }
