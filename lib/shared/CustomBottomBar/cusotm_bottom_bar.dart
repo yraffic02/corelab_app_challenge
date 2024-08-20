2a1545718de9162da4b63eda47c1a55677f7f2c8
yraffic02 <yraffic02@gmail.com>
2024-08-20 14:48:27 -0300

    feat: criado custom widget

---
+++ lib/shared/CustomBottomBar/cusotm_bottom_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomBar extends StatefulWidget {
+  const CustomBottomBar({super.key});
+
+  @override
+  State<CustomBottomBar> createState() => _CustomBottomBarState();
+}
+
+class _CustomBottomBarState extends State<CustomBottomBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/Categorias');
+        break;
+    }
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final currentRoute = ModalRoute.of(context)?.settings.name;
+    if (currentRoute == '/') {
+      _selectedIndex = 0;
+    } else if (currentRoute == '/Categorias') {
+      _selectedIndex = 1;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.category),
+          label: 'Categorias',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box),
+          label: 'Anunciar',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Favoritos',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Conta',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: const Color(0xFF00B4CC),
+      unselectedItemColor: const Color(0xFF7C9094),
+      onTap: _onItemTapped,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+}
--- lib/shared/CustomLayout/custom_layout.dart
+++ lib/shared/CustomLayout/custom_layout.dart
@@ -1,4 +1,5 @@
 import 'package:corelab_app_challenge/shared/CustomAppbar/custom_appbar.dart';
+import 'package:corelab_app_challenge/shared/CustomBottomBar/cusotm_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class CustomLayout extends StatefulWidget {
@@ -11,48 +12,12 @@ class CustomLayout extends StatefulWidget {
 }
 
 class _CustomLayoutState extends State<CustomLayout> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CustomAppbar(),
-      body: widget.child,
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            label: 'Categorias',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box),
-            label: 'Anunciar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favoritos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Conta',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color(0xFF00B4CC),
-        unselectedItemColor: const Color(0xFF7C9094),
-        onTap: _onItemTapped,
-      ),
-    );
+        backgroundColor: Colors.white,
+        appBar: const CustomAppbar(),
+        body: widget.child,
+        bottomNavigationBar: const CustomBottomBar());
   }
 }
