06469958a5cfdd39f65f18c1a055fafae58e2f0f
yraffic02 <yraffic02@gmail.com>
2024-08-20 21:31:35 -0300

    chore: add props

--- lib/shared/CustomLayout/custom_layout.dart
+++ lib/shared/CustomLayout/custom_layout.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:corelab_app_challenge/shared/CustomAppbar/custom_appbar.dart';
 import 'package:corelab_app_challenge/shared/CustomBottomBar/cusotm_bottom_bar.dart';
-import 'package:flutter/material.dart';
 
 class CustomLayout extends StatefulWidget {
   final Widget child;
+  final FocusNode? focusNode;
+  final TextEditingController? searchController;
 
-  const CustomLayout({super.key, required this.child});
+  const CustomLayout({
+    super.key,
+    required this.child,
+    this.focusNode,
+    this.searchController,
+  });
 
   @override
   State<CustomLayout> createState() => _CustomLayoutState();
@@ -15,9 +22,13 @@ class _CustomLayoutState extends State<CustomLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: const CustomAppbar(),
-        body: widget.child,
-        bottomNavigationBar: const CustomBottomBar());
+      backgroundColor: Colors.white,
+      appBar: CustomAppbar(
+        focusNode: widget.focusNode,
+        searchController: widget.searchController,
+      ),
+      body: widget.child,
+      bottomNavigationBar: const CustomBottomBar(),
+    );
   }
 }
