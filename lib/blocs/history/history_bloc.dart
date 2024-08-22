7ab4e1f2940d45ffb12f93a3cfe90141846f6779
yraffic02 <yraffic02@gmail.com>
2024-08-21 22:56:56 -0300

    chore: passando a logica de adicionar historico para bloc

--- lib/blocs/history/history_bloc.dart
+++ lib/blocs/history/history_bloc.dart
@@ -11,16 +11,22 @@ class HistoryBloc extends Bloc<HistoryEvent, HistoryState> {
 
   void _mapEventToState(HistoryEvent event, Emitter emit) async {
     final prefs = await SharedPreferences.getInstance();
+    List<String> historyStrings = prefs.getStringList('search_history') ?? [];
     List<HistoryModel> historys = [];
 
     emit(HistoryLoadingState());
 
     if (event is GetHistory) {
-      List<String> historyStrings = prefs.getStringList('search_history') ?? [];
-
       historys = historyStrings
           .map((history) => HistoryModel(history: history))
           .toList();
+    }
+
+    if (event is AddHistory) {
+      if (!historyStrings.contains(event.history)) {
+        historyStrings.add(event.history);
+        await prefs.setStringList('search_history', historyStrings);
+      }
     }
 
     emit(HistoryLoadedState(historys: historys));
--- lib/blocs/history/history_event.dart
+++ lib/blocs/history/history_event.dart
@@ -1,3 +1,9 @@
 abstract class HistoryEvent {}
 
 class GetHistory extends HistoryEvent {}
+
+class AddHistory extends HistoryEvent {
+  final String history;
+
+  AddHistory({required this.history});
+}
